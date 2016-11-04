5b4de58da73aa5d70d03249bd1407f6a2212e2c1
Zach Silveira <zackify@gmail.com>
2016-11-04 11:20:25 -0400

    Fix tests for running on a new db

--- tests/unit/all.js
+++ tests/unit/all.js
@@ -2,8 +2,9 @@ import Chat from '../chat'
 
 describe('Model.all()', () => {
   it('grabs all', async function() {
+    await Chat.create({ messages: 'blah' })
     let chats = await Chat.all()
-    expect(chats.length).to.be.above(1)
+    expect(chats.length).to.be.above(0)
     expect(typeof chats).to.equal('object')
   })
 })
--- tests/unit/where.js
+++ tests/unit/where.js
@@ -2,7 +2,7 @@ import Chat from '../chat'
 
 describe('Model.where()', () => {
   it('grabs by id', async function() {
-    let chats = await Chat.where({ id: 2 })
+    let chats = await Chat.where({ id: 1 })
     expect(chats.length).to.be.equal(1)
     expect(typeof chats).to.equal('object')
   })
